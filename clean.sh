388ab2a5bad29bf2a2c1f21fa7252e0ffb430238
Mikhail Mitrofanov <mikhail404@gmail.com>
2015-10-03 00:22:57 +0300

    Added to the basic structure of au_git project

---
+++ clean.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+make clean
+rm Makefile cmake_install.cmake CMakeCache.txt -rf CMakeFiles
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+/*
+ *  Mikhail Mitrofanov, SPbAU 2015 (c)
+ */
+ 
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Hello, au_git!" << std::endl;
+    return 0;
+}
