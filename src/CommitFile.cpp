4e933b8e7662344c7ee68b91ef7ffaa0c56565c5
Mikhail Mitrofanov <mikhail404@gmail.com>
2015-10-26 22:19:30 +0300

    Add files func to local repository

--- src/CommitFile.cpp
+++ src/CommitFile.cpp
@@ -1,10 +1,24 @@
-#include <CommitException.hpp>
+#include "RepositoryException.hpp"
 #include "CommitFile.hpp"
 
 #include <boost/format.hpp>
+#include <fstream>
 
-CommitFile::CommitFile(const std::string &filename)
-        : m_filename(filename), m_timestamp(std::time_t(0))
+const std::string storage_path = "/home/mikhail/objects/";
+
+bool clone_file(boost::filesystem::path in_file, boost::filesystem::path out_file)
+{
+    std::ifstream in(in_file.string(), std::ios::binary);
+    std::ofstream out(out_file.string(), std::ios::binary);
+    out << in.rdbuf();
+    return in && out;
+}
+
+CommitFile::CommitFile()
+{
+}
+
+CommitFile::CommitFile(const std::string &filename) : m_filename(filename), m_timestamp(std::time_t(0))
 {
     if (!boost::filesystem::exists(m_filename))
     {
@@ -12,7 +26,20 @@ CommitFile::CommitFile(const std::string &filename)
         throw FileIsNotFoundException(message.c_str());
     }
     m_timestamp = boost::filesystem::last_write_time(m_filename);
-    m_hash_code_file.set_hashcode(m_filename);
+    m_hash_code_file.set_hash_code(m_filename);
+}
+
+void CommitFile::add_to_storage() const
+{
+    std::ifstream file(m_filename, std::ios::binary);
+    std::string hash_code_file = encode_content_file(file).to_string();
+    boost::filesystem::path in_file(m_filename);
+    boost::filesystem::path out_file(boost::filesystem::path(storage_path).string() + hash_code_file);
+    if (!clone_file(in_file, out_file))
+    {
+        std::string message = boost::str(boost::format("File [%1%] is not commited!") % m_filename);
+        throw FileIsNotCommitedException(message.c_str());
+    }
 }
 
 std::string CommitFile::filename() const
