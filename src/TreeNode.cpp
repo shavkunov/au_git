d0cc9abc0a6b86b4e89c086ed55a9c5578aba486
Mikhail Mitrofanov <mikhail404@gmail.com>
2015-10-26 22:22:54 +0300

    simple fixs

--- src/TreeNode.cpp
+++ src/TreeNode.cpp
@@ -1,19 +1,8 @@
 #include "TreeNode.hpp"
 
-TreeNode::TreeNode(Commit commit, size_t position, TreeNode *parent_node)
+TreeNode::TreeNode(Commit commit, /*size_t position,*/ std::shared_ptr <TreeNode> parent_node)
     : m_commit_value(commit),
-      m_index_position(position),
+//      m_index_position(position),
       m_parent_node(parent_node)
 {
-}
-
-void TreeNode::destroy_chain(TreeNode* node)
-{
-    while (node != nullptr)
-    {
-        TreeNode* temp_node = node;
-        node = node->m_parent_node;
-        delete temp_node;
-        temp_node = nullptr;
-    }
 }
