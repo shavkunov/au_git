d601129c2ba3e253f2f15ef68c46a821580754f7
Mikhail Mitrofanov <mikhail404@gmail.com>
2015-10-26 22:18:56 +0300

    Added the ability to add commit-files to the local repo

--- src/Commit.cpp
+++ src/Commit.cpp
@@ -1,9 +1,4 @@
-//
-// Created by mikhail on 10/10/15.
-//
-
 #include "Commit.hpp"
-#include "CommitException.hpp"
 
 Commit::Commit()
 {
@@ -12,18 +7,17 @@ Commit::Commit()
 
 void Commit::add_files(const std::vector <CommitFile> &commit_files)
 {
-    //for(CommitFile current_commit_file : commit_files)
-    {
-        //if (!boost::filesystem::exists(current_commit_file.filename()))
-        {
-            ;//throw FileIsNotFoundException("File is not found");
-        }
-    }
-    // ?
     m_commit_files.insert(m_commit_files.end(), commit_files.begin(), commit_files.end());
+    m_hash_code_commit.set_hash_code_by_list(commit_files);
 }
 
-void Commit::set_parent_commit(const HashCodeType parent_commit_type)
+void Commit::add_to_storage() const
+{
+    for(CommitFile commit_file : m_commit_files)
+        commit_file.add_to_storage();
+}
+
+void Commit::set_parent_hash_code_commit(const HashCodeType parent_commit_type)
 {
     m_parent_hash_code = parent_commit_type;
 }
